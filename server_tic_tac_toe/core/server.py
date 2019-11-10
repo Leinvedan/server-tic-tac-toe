b7972d362a32335ebb5c7bd28b86d141efb7c46b
Leinvedan <fontebasso.lemos@gmail.com>
2019-11-09 21:11:34 -0300

    refactor: move open connection to a function

--- server_tic_tac_toe/core/server.py
+++ server_tic_tac_toe/core/server.py
@@ -9,22 +9,31 @@ PORT = 5332
 logger = create_logger(name="SERVER", color="CYAN")
 
 
-try:
+def open_tcp_server_socket():
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     tcp.bind((HOST, PORT))
     tcp.listen(1)
-    logger.info('Listening...')
+    return tcp
 
-    while True:
+
+tcp = open_tcp_server_socket()
+logger.info('Listening...')
+while True:
+    try:
         connection, client = tcp.accept()
         logger.info(client)
         connection_handler = Thread(
-            target=handle_connection, args=(connection, client), daemon=True)
+            target=handle_connection,
+            args=(connection, client),
+            daemon=True
+        )
         connection_handler.start()
+    except KeyboardInterrupt:
+        tcp.shutdown(socket.SHUT_RDWR)
+        tcp.close()
+        logger.info("Exiting...")
+        break
 
-except KeyboardInterrupt:
-    tcp.shutdown(socket.SHUT_RDWR)
-    tcp.close()
-    logger.info("Exiting...")
+

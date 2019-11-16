e27bcc294b7aedba5a5569253850e61fd7f72cfa
Leinvedan <fontebasso.lemos@gmail.com>
2019-11-15 21:36:06 -0300

    feat: update client to match new protocol

--- server_tic_tac_toe/client/main.py
+++ server_tic_tac_toe/client/main.py
@@ -1,12 +1,21 @@
 import socket
+import json
 
 from server_tic_tac_toe.config import HOST, PORT
 
 # testing client
 
 
-def read_input_as_bytes():
-    return bytes(input(">"), encoding='utf8')
+def read_coordinates_input():
+    command = input(">").split(' ')
+    command = {'line': command[0], 'column': command[1]}
+    command = json.dumps(command)
+    return bytes(command, encoding='utf8')
+
+
+def read_input():
+    command = input(">")
+    return bytes(command, encoding='utf8')
 
 
 try:
@@ -15,12 +24,22 @@ try:
     tcp.connect(dest)
 
     print("Write message:")
-    message_to_send = read_input_as_bytes()
+
+    #  name confirmation
+    message_to_send = read_input()
+    tcp.sendall(message_to_send)
+    response = tcp.recv(1024)
+    print("server:", response.decode('utf8'))
+
     while message_to_send != "quit":
-        tcp.sendall(message_to_send)
         response = tcp.recv(1024)
         print("server:", response.decode('utf8'))
-        message_to_send = read_input_as_bytes()
+        response = json.loads(response)
+        if (response['status'] == 'play'):
+            # send play
+            message_to_send = read_coordinates_input()
+            tcp.sendall(message_to_send)
+
 except KeyboardInterrupt:
     print("Exiting...")
 finally:

5fa30d2a3db18142c906a2d0c5fef293cc770453
Leinvedan <fontebasso.lemos@gmail.com>
2019-11-16 22:38:52 -0300

    remove try catch to make testing code cleaner

--- server_tic_tac_toe/client/main.py
+++ server_tic_tac_toe/client/main.py
@@ -10,49 +10,43 @@ from server_tic_tac_toe.client.parsing import (
 
 # testing client
 
-try:
-    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp.connect((HOST, PORT))
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp.connect((HOST, PORT))
 
-    print("Write your name:")
+print("Write your name:")
 
-    #  name confirmation
-    message_to_send = read_name_input()
-    tcp.sendall(message_to_send)
+#  name confirmation
+message_to_send = read_name_input()
+tcp.sendall(message_to_send)
 
-    while message_to_send != "quit":
-        response = tcp.recv(1024)
-        response = response.decode('utf8')
-        response = json.loads(response)
-        pretty_print_board(response)
+while message_to_send != "quit":
+    response = tcp.recv(1024)
+    response = response.decode('utf8')
+    response = json.loads(response)
+    pretty_print_board(response)
 
-        if response['status'] == 'matched':
-            print('Matched with other player!')
+    if response['status'] == 'matched':
+        print('Matched with other player!')
 
-        elif response['status'] == 'play':
-            # send play
+    elif response['status'] == 'play':
+        # send play
+        message_to_send = read_coordinates_input()
+        tcp.sendall(message_to_send)
+
+    elif response['status'] == 'waiting':
+        print("Waiting match")
+
+    elif response['status'] == 'error':
+        print('[SERVER]:', response['message'])
+
+        if response['error_type'] == 'INVALID_FORMAT':
+            print('invalid format check the client`s code')
+            exit()
+
+        if response['error_type'] == 'INVALID_VALUES':
+            print('invalid input, try again')
             message_to_send = read_coordinates_input()
             tcp.sendall(message_to_send)
 
-        elif response['status'] == 'waiting':
-            print("Waiting match")
-
-        elif response['status'] == 'error':
-            print('[SERVER]:', response['message'])
-
-            if response['error_type'] == 'INVALID_FORMAT':
-                print('invalid format check the client`s code')
-                exit()
-
-            if response['error_type'] == 'INVALID_VALUES':
-                print('invalid input, try again')
-                message_to_send = read_coordinates_input()
-                tcp.sendall(message_to_send)
-
-            elif response['error_type'] == 'OPPONENT_LEFT':
-                print('Waiting new game')
-
-except KeyboardInterrupt:
-    print("Exiting...")
-finally:
-    tcp.close()
+        elif response['error_type'] == 'OPPONENT_LEFT':
+            print('Waiting new game')
